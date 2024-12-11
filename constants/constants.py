be547a822da4a282ab2975f8a6a76ef0856112aa
Aman Verma <amananimator6@gmail.com>
2024-12-11 23:51:26 +0530

    working on layouting and setting few icons

--- constants/constants.py
+++ constants/constants.py
@@ -15,14 +15,30 @@ ADD_ICON = ADD_ICON.as_posix()
 NUKE_ICON = icons_path / 'nuke_red_icon.png'
 NUKE_ICON = NUKE_ICON.as_posix()
 
-PLAY_ICON = icons_path / 'PLAY_ICON.png'
+PLAY_ICON = icons_path / 'play_icon.png'
 PLAY_ICON = PLAY_ICON.as_posix()
 
-REMOVE_ICON = icons_path / 'REMOVE_ICON.png'
+STOP_ICON = icons_path / 'stop_icon.png'
+STOP_ICON = STOP_ICON.as_posix()
+
+REMOVE_ICON = icons_path / 'remove_icon.png'
 REMOVE_ICON = REMOVE_ICON.as_posix()
 
-REMOVE_SELECTED_ICON = icons_path / 'REMOVE_SELECTED_ICON.png'
+REMOVE_SELECTED_ICON = icons_path / 'remove_selected_icon.png'
 REMOVE_SELECTED_ICON = REMOVE_SELECTED_ICON.as_posix()
 
-USER_ICON = icons_path / 'USER_ICON.png'
+USER_ICON = icons_path / 'user_icon.png'
 USER_ICON = USER_ICON.as_posix()
+
+OPERATION_PLAY_ICON = icons_path / 'operation_play_icon.png'
+OPERATION_PLAY_ICON = OPERATION_PLAY_ICON.as_posix()
+
+OPERATION_STOP_ICON = icons_path / 'operation_stop_icon.png'
+OPERATION_STOP_ICON = OPERATION_STOP_ICON.as_posix()
+
+OPEN_DIR_ICON = icons_path / 'opne_dir_icon.png'
+OPEN_DIR_ICON = OPEN_DIR_ICON.as_posix()
+
+RV_ICON = icons_path / 'rv_icon.png'
+RV_ICON = RV_ICON.as_posix()
+
--- ui.py
+++ ui.py
@@ -6,12 +6,12 @@ from pathlib import Path
 import subprocess
 
 # Importing third party modules
-from PySide2.QtWidgets import QApplication, QMainWindow, QProgressBar, QMenuBar, QWidget, QPushButton, QVBoxLayout,  QHBoxLayout, QLabel, QTableWidget, QFileDialog, QTableWidgetItem, QHeaderView
+from PySide2.QtWidgets import QApplication, QSpacerItem,QSizePolicy, QMainWindow, QProgressBar, QMenuBar, QWidget, QPushButton, QVBoxLayout,  QHBoxLayout, QLabel, QTableWidget, QFileDialog, QTableWidgetItem, QHeaderView
 from PySide2.QtGui import QPixmap, QIcon
 from PySide2.QtCore import Qt , QSize
 
 # Importing custom modules
-from constants.constants import NUKE_ICON, USER_ICON, ADD_ICON, REMOVE_ICON, REMOVE_SELECTED_ICON, PLAY_ICON
+from constants.constants import NUKE_ICON, USER_ICON, ADD_ICON, REMOVE_ICON, REMOVE_SELECTED_ICON, PLAY_ICON, STOP_ICON, OPERATION_PLAY_ICON, OPERATION_STOP_ICON, OPEN_DIR_ICON, RV_ICON
 
 
 
@@ -26,10 +26,10 @@ class RenderMate(QMainWindow):
 
 
         self.setWindowTitle("RenderMate V1.0.0")
-        self.setFixedSize(1600, 800)
-        self.RV_playerMenu = QMenuBar()        
-        self.RV_playerMenu.addMenu("RV Player Path")
-        self.setMenuBar(self.RV_playerMenu)
+        self.setMinimumSize(1600, 800)
+        self.menu_bar = QMenuBar()        
+        self.set_path = self.menu_bar.addMenu("Set Path")
+        self.setMenuBar(self.menu_bar)
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
 
@@ -61,45 +61,190 @@ class RenderMate(QMainWindow):
         #############################################################################################################################################
 
         # Sidebar widget setup
-        self.side_bar = QWidget()
-        self.side_bar.setFixedWidth(100)
+        self.side_bar_widget = QWidget()
+        self.side_bar_widget.setFixedWidth(105)
         # self.side_bar.setStyleSheet("background-color: #323232;")
         
-        # Initialize buttons for sidebar
+        # Create buttons
         self.add_button = QPushButton()
-        self.remove_selected = QPushButton("Remove Selected")
-        self.remove_all = QPushButton("Remove all")
-        self.start_all = QPushButton("Start all")
-        self.stop_all = QPushButton("stop all")
-        
+        self.add_button.setFlat(True)
+        self.remove_selected = QPushButton()
+        self.remove_all = QPushButton()
+        self.start_all = QPushButton()
+        self.stop_all = QPushButton()
 
-        self.add_button.setStyleSheet("QPushButton { border: none; }")
-        self.add_button.setIconSize(QSize(48, 48)) 
 
-        # set buttons icons
-        self.add_button.setIcon(QIcon(ADD_ICON))
-        self.start_all.setIcon(QIcon(PLAY_ICON))
-        # self.stop_all.setIcon(stop)
-        self.remove_all.setIcon(QIcon(REMOVE_ICON))
-        self.remove_selected.setIcon(QIcon(REMOVE_SELECTED_ICON))
 
-        #adding functions to buttons
         self.add_button.clicked.connect(self.add_files_to_table)
         self.remove_all.clicked.connect(self.clear_table)
         self.remove_selected.clicked.connect(self.remove_selected_rows)
 
-        # Layout for sidebar buttons
-        self.buttons_layout = QVBoxLayout()
-        self.buttons_layout.addWidget(self.add_button)
-        self.buttons_layout.addWidget(self.remove_all)
-        self.buttons_layout.addWidget(self.remove_selected)
-        self.buttons_layout.addWidget(self.start_all)
-        self.buttons_layout.addWidget(self.stop_all)
+        # set buttons icons
+        self.add_button.setIcon(QIcon(ADD_ICON))
+        self.start_all.setIcon(QIcon(PLAY_ICON))
+        self.stop_all.setIcon(QIcon(STOP_ICON))
+        self.remove_all.setIcon(QIcon(REMOVE_ICON))
+        self.remove_selected.setIcon(QIcon(REMOVE_SELECTED_ICON))
+
+
+
+        self.add_button.setIconSize(QSize(40, 40)) 
+        self.remove_selected.setIconSize(QSize(40, 40)) 
+        self.remove_all.setIconSize(QSize(60, 40)) 
+        self.start_all.setIconSize(QSize(40, 40)) 
+        # self.stop_all.setStyleSheet("QPushButton { border: none; }") 
+        self.stop_all.setIconSize(QSize(40, 40)) 
         
+
+
+        # Create labels for buttons
+        self.add_label = QLabel("Add")
+        self.add_label.setFixedHeight(40)
+        self.remove_all_label = QLabel("Remove all")
+        self.remove_selected_label = QLabel("Remove Selected")
+        self.start_all_label = QLabel("Start all")
+        self.stop_all_label = QLabel("Stop all")
         
+        self.add_label.setFixedHeight(20)
+        self.remove_selected_label.setFixedHeight(30)
+        self.remove_all_label.setFixedHeight(10)
+        self.start_all_label.setFixedHeight(20)
+        self.stop_all_label.setFixedHeight(20)
+
         
+        self.add_label.setAlignment(Qt.AlignCenter)
+        self.remove_selected_label.setAlignment(Qt.AlignCenter)
+        self.remove_all_label.setAlignment(Qt.AlignCenter)
+        self.start_all_label.setAlignment(Qt.AlignCenter)
+        self.stop_all_label.setAlignment(Qt.AlignCenter)
+
+
+
+
+
+
+
+
+
+
+
+
+        # Create individual layouts for each button and label pair
+        self.add_layout = QVBoxLayout()
+        self.add_layout.setSpacing(0)
+        self.add_layout.addWidget(self.add_button)
+        self.add_layout.addWidget(self.add_label)
+
+        self.remove_all_layout = QVBoxLayout()
+        self.remove_all_layout.setSpacing(0)
+        self.remove_all_layout.addWidget(self.remove_all)
+        self.remove_all_layout.addWidget(self.remove_all_label)
+
+        self.remove_selected_layout = QVBoxLayout()
+        self.remove_selected_layout.setSpacing(0)
+        self.remove_selected_layout.addWidget(self.remove_selected)
+        self.remove_selected_layout.addWidget(self.remove_selected_label)
+
+        self.start_all_layout = QVBoxLayout()
+        self.start_all_layout.setSpacing(0)
+        self.start_all_layout.addWidget(self.start_all)
+        self.start_all_layout.addWidget(self.start_all_label)
+        
+
+        self.stop_all_layout = QVBoxLayout()
+        self.stop_all_layout.setSpacing(0)
+        self.stop_all_layout.addWidget(self.stop_all)
+        self.stop_all_layout.addWidget(self.stop_all_label)
+
+        # Create the main layout and add each button layout
+        self.side_bar_main_layout = QVBoxLayout()
+        self.side_bar_main_layout.addLayout(self.add_layout)
+        self.side_bar_main_layout.addLayout(self.remove_all_layout)
+        self.side_bar_main_layout.addLayout(self.remove_selected_layout)
+        self.side_bar_main_layout.addLayout(self.start_all_layout)
+        self.side_bar_main_layout.addLayout(self.stop_all_layout)
+        
+        # Create a spacer item with a fixed height
+        spacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        # Add the spacer before the sidebar widget in the layout
+        self.side_bar_main_layout.addItem(spacer)
         # set Layout for sidebar buttons
-        self.side_bar.setLayout(self.buttons_layout)
+        self.side_bar_widget.setLayout(self.side_bar_main_layout)
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+        # # Initialize buttons for sidebar
+        # self.add_button = QPushButton()
+        # self.remove_selected = QPushButton("Remove Selected")
+        # self.remove_all = QPushButton("Remove all")
+        # self.start_all = QPushButton("Start all")
+        # self.stop_all = QPushButton("stop all")
+        
+
+        # # self.add_button.setStyleSheet("QPushButton { border: none; }")
+        # self.add_button.setIconSize(QSize(48, 48)) 
+
+        # # set buttons icons
+        # self.add_button.setIcon(QIcon(ADD_ICON))
+        # self.start_all.setIcon(QIcon(PLAY_ICON))
+        # # self.stop_all.setIcon(stop)
+        # self.remove_all.setIcon(QIcon(REMOVE_ICON))
+        # self.remove_selected.setIcon(QIcon(REMOVE_SELECTED_ICON))
+
+        # #adding functions to buttons
+        # self.add_button.clicked.connect(self.add_files_to_table)
+        # self.remove_all.clicked.connect(self.clear_table)
+        # self.remove_selected.clicked.connect(self.remove_selected_rows)
+
+        # # Layout for sidebar buttons
+        # self.buttons_layout = QVBoxLayout()
+        # self.buttons_layout.addWidget(self.add_button)
+        # self.buttons_layout.addWidget(self.remove_all)
+        # self.buttons_layout.addWidget(self.remove_selected)
+        # self.buttons_layout.addWidget(self.start_all)
+        # self.buttons_layout.addWidget(self.stop_all)
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -119,7 +264,7 @@ class RenderMate(QMainWindow):
         
         # Table and sidebar layout setup
         self.hbox_layout = QHBoxLayout()
-        self.hbox_layout.addWidget(self.side_bar)
+        self.hbox_layout.addWidget(self.side_bar_widget)
         self.hbox_layout.addWidget(self.table_widget)
 
         # Main layout setup
@@ -142,10 +287,14 @@ class RenderMate(QMainWindow):
             """
             self.operation_row_widget = QWidget()
 
-            self.start_button = QPushButton("Start")
+            self.start_button = QPushButton("")
+            self.start_button.setIcon(QIcon(OPERATION_PLAY_ICON))
+            self.start_button.setIconSize(QSize(40, 40)) 
             self.start_button.setMinimumHeight(40)
 
-            self.stop_button = QPushButton("Stop")
+            self.stop_button = QPushButton("")
+            self.stop_button.setIcon(QIcon(OPERATION_STOP_ICON))
+            self.stop_button.setIconSize(QSize(40, 40)) 
             self.stop_button.setMinimumHeight(40)
 
             # Create horizontal layout for buttons
@@ -171,14 +320,23 @@ class RenderMate(QMainWindow):
             self.launcher_row_widget = QWidget()
 
             # Initialize buttons
-            self.rv_button = QPushButton("RV")
+            self.rv_button = QPushButton("")
+            self.rv_button.setIcon(QIcon(RV_ICON))
+            self.rv_button.setIconSize(QSize(40, 40)) 
+            self.rv_button.setFlat(True)
             self.rv_button.setMinimumHeight(40)
 
-            self.nuke_button = QPushButton("Nuke")
+            self.nuke_button = QPushButton("")
+            self.nuke_button.setIcon(QIcon(NUKE_ICON))
+            self.nuke_button.setFlat(True)
+            self.nuke_button.setIconSize(QSize(40, 40)) 
             self.nuke_button.clicked.connect(self.open_nuke_file)
             self.nuke_button.setMinimumHeight(40)
 
-            self.open_render_dir_button = QPushButton("Open Render dir")
+            self.open_render_dir_button = QPushButton("")
+            self.open_render_dir_button.setFlat(True)
+            self.open_render_dir_button.setIcon(QIcon(OPEN_DIR_ICON))
+            self.open_render_dir_button.setIconSize(QSize(40, 40)) 
             self.open_render_dir_button.setMinimumHeight(40)
 
             # Create horizontal layout for buttons
